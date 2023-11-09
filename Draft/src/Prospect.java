ce83a82ccfa4485880220bc09368ff9ee979ec5b
zackwilliams10 <zdanielwilliams10@gmail.com>
2023-11-09 16:30:01 -0500

    added setName to Prospect

--- Draft/src/Prospect.java
+++ Draft/src/Prospect.java
@@ -1,4 +1,17 @@
 
 public class Prospect {
-
+	private String playerName;
+	private String school;
+	
+	
+	public void setName(String n) {
+		playerName = n;
+		
+	}
+	
+	public String getName() {
+		return playerName;
+	}
+	
+	
 }
