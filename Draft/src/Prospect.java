1afc99e62b6d00e6aa109e310a449db25b8e0203
zackwilliams10 <zdanielwilliams10@gmail.com>
2023-11-16 16:15:04 -0500

    Pushing Harry's version

--- Draft/src/Prospect.java
+++ Draft/src/Prospect.java
@@ -82,29 +82,4 @@ public class Prospect {
 
 }
 
-public class Prospect {
-	private String playerName;
-	private String school;
 
-	
-	
-	
-	
-	public void setName(String n) {
-		playerName = n;
-		
-	}
-	
-	public String getName() {
-		return playerName;
-	}
-	
-	public void setSchool(String s){
-		school = s;
-	}
-
-	public String getSchool(){
-		return school;
-	}
-	
-}
